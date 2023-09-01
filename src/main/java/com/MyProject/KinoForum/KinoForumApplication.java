747499b22661805826518b42978c0c846d6f9b6f
zhenya93 <zhenya939393@yandex.ru>
2023-09-01 21:42:02 +0300

    initial

---
+++ src/main/java/com/MyProject/KinoForum/KinoForumApplication.java
@@ -0,0 +1,13 @@
+package com.MyProject.KinoForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyProject/KinoForum/KinoForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyProject.KinoForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
